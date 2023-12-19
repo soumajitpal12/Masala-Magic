ff74076ca894e3c0d280bfbe37665163e072cf57
soumajitpal12 <soumajitpal12@gamil.com>
2023-12-19 13:07:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("Wellcome To My Restaurant")
+var responsive_nav = document.getElementById("responsive_nav") 
+
+function openmenu(){
+    responsive_nav.style.left = "0";
+}
+
+function closemenu(){
+    responsive_nav.style.left = "-40vw";
+}
